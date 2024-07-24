c152ba00ddee99b674d5cfe104696c2fc33d3016
Mostafa Masri <moustafa.masri.1522@gmail.com>
2024-07-24 21:26:03 +0400

    add apks

--- lib/features/auth/presentation/cubits/auth/auth_bloc.dart
+++ lib/features/auth/presentation/cubits/auth/auth_bloc.dart
@@ -9,17 +9,23 @@ import '../../../../../core/common/cubit/app_user/app_user_cubit.dart';
 import '../../../../../core/common/entites/user.dart';
 import '../../../../../core/usecase/usecase.dart';
 import '../../../domain/usecases/signout_usecase.dart';
+
 part 'auth_event.dart';
 part 'auth_state.dart';
 part 'auth_bloc.freezed.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  // Use cases for handling authentication
   final SignUpUsecase _signupUsecase;
   final SignInUsecase _signinUsecase;
   final GetUserInfoUsecase _getCurrentUserUsecase;
   final UserStreamUsecase _userStreamUsecase;
   final SignoutUsecase _signoutUsecase;
+
+  // Cubit for updating app user state
   final AppUserCubit _appUserCubit;
+
+  // Constructor to initialize use cases and cubit
   AuthBloc({
     required UserStreamUsecase userStreamUsecase,
     required SignUpUsecase signupUsecase,
@@ -34,29 +40,37 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         _getCurrentUserUsecase = getCurrentUserUsecase,
         _appUserCubit = appUserCubit,
         super(const AuthState.initial()) {
+    // Handling different authentication events
     on<_AuthSignUp>(_signup);
     on<_AuthSignIn>(_signin);
     on<_AuthSignOut>(_signout);
     on<_AuthGetUserInfo>(_getCurrentUser);
   }
 
-  void _signin(event, emit) async {
+  // Handles the sign-in event
+  void _signin(_AuthSignIn event, Emitter<AuthState> emit) async {
     emit(const AuthState.loading());
+
+    // Call the sign-in use case with provided email and password
     final res = await _signinUsecase(
       SignInParams(
         email: event.email,
         password: event.password,
       ),
     );
+
+    // Emit state based on result of the sign-in attempt
     res.fold(
       (error) => emit(AuthState.failed(error.message)),
       (user) => _emitUserState(user, emit),
     );
   }
 
-  void _signup(event, emit) async {
+  // Handles the sign-up event
+  void _signup(_AuthSignUp event, Emitter<AuthState> emit) async {
     emit(const AuthState.loading());
 
+    // Call the sign-up use case with provided user details
     var res = await _signupUsecase(
       SignUpParams(
         name: event.name,
@@ -64,20 +78,23 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         password: event.password,
       ),
     );
+
+    // Emit state based on result of the sign-up attempt
     res.fold(
       (error) => emit(AuthState.failed(error.message)),
       (user) => _emitUserState(user, emit),
     );
   }
 
-  void _getCurrentUser(event, Emitter<AuthState> emit) async {
+  // Handles getting current user info
+  void _getCurrentUser(_AuthGetUserInfo event, Emitter<AuthState> emit) async {
     // Emit loading state
     emit(const AuthState.loading());
 
-    // Listen to the user stream
+    // Listen to the user stream for authentication state changes
     await _userStreamUsecase().listen((user) async {
       if (user != null) {
-        // Get the current user details
+        // If a user is found, get their details
         final res = await _getCurrentUserUsecase(NoParams());
         res.fold(
           (error) {
@@ -91,17 +108,20 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     }).asFuture();
   }
 
+  // Emits user-related state and updates the app user cubit
   void _emitUserState(User user, Emitter<AuthState> emit) {
     _appUserCubit.updateUser(user);
-
     emit(AuthState.success(user));
   }
 
+  // Handles the sign-out event
   void _signout(_AuthSignOut event, Emitter<AuthState> emit) async {
     emit(const AuthState.loading());
 
+    // Call the sign-out use case
     final res = await _signoutUsecase(NoParams());
 
+    // Emit state based on result of the sign-out attempt
     res.fold(
       (error) => emit(AuthState.failed(error.message)),
       (message) {
--- lib/features/cart/presentation/bloc/cart/cart_bloc.dart
+++ lib/features/cart/presentation/bloc/cart/cart_bloc.dart
@@ -15,40 +15,48 @@ part 'cart_state.dart';
 part 'cart_bloc.freezed.dart';
 
 class CartBloc extends Bloc<CartEvent, CartState> {
+  // Use cases for managing cart items
   final GetCartItemsUsecase _getCartItemsUsecase;
   final ClearCartUsecase _clearCartUsecase;
   final SaveCartUsecase _saveCartUsecase;
+
+  // Constructor initializes use cases and sets initial state
   CartBloc(
     this._getCartItemsUsecase,
     this._clearCartUsecase,
     this._saveCartUsecase,
   ) : super(const CartState.initial()) {
+    // Event handlers
     on<_GetCartItems>(_getCartItems);
     on<_AddToCart>(_addToCart);
     on<_ClearCart>(_clearCart);
     on<_RemoveFromCart>(_removeFromCart);
   }
 
+  // Handler for fetching cart items
   FutureOr<void> _getCartItems(
       _GetCartItems event, Emitter<CartState> emit) async {
-    emit(const CartState.loading());
-    final result = await _getCartItemsUsecase(NoParams());
+    emit(const CartState.loading()); // Emit loading state
+    final result = await _getCartItemsUsecase(NoParams()); // Fetch cart items
     result.fold(
       (error) {
-        emit(CartState.error(message: error.message));
+        emit(CartState.error(message: error.message)); // Emit error state
       },
       (cartItems) {
-        emit(CartState.loaded(cartItems: cartItems));
+        emit(CartState.loaded(cartItems: cartItems)); // Emit loaded state
       },
     );
   }
 
+  // Handler for adding a product to the cart
   FutureOr<void> _addToCart(_AddToCart event, Emitter<CartState> emit) async {
     state.whenOrNull(
       loaded: (cartItems) async {
+        // Check if the product is already in the cart
         if (!cartItems.any(
           (element) => element.productId == event.product.id,
         )) {
+          // Create a new cart item
           final newCartItems = [
             ...cartItems,
             CartItem(
@@ -60,13 +68,15 @@ class CartBloc extends Bloc<CartEvent, CartState> {
             )
           ];
 
-          final result = await _saveCartUsecase(newCartItems);
+          final result =
+              await _saveCartUsecase(newCartItems); // Save updated cart
           result.fold(
             (error) {
-              emit(CartState.error(message: error.message));
+              emit(CartState.error(message: error.message)); // Emit error state
             },
             (cartItems) {
-              emit(CartState.loaded(cartItems: newCartItems));
+              emit(CartState.loaded(
+                  cartItems: newCartItems)); // Emit loaded state
             },
           );
         }
@@ -74,32 +84,38 @@ class CartBloc extends Bloc<CartEvent, CartState> {
     );
   }
 
+  // Handler for clearing all items in the cart
   FutureOr<void> _clearCart(_ClearCart event, Emitter<CartState> emit) async {
-    final result = await _clearCartUsecase.call(NoParams());
+    final result = await _clearCartUsecase.call(NoParams()); // Clear cart
     result.fold(
       (error) {
-        emit(CartState.error(message: error.message));
+        emit(CartState.error(message: error.message)); // Emit error state
       },
       (cartItems) {
-        emit(const CartState.loaded(cartItems: []));
+        emit(const CartState.loaded(
+            cartItems: [])); // Emit loaded state with empty cart
       },
     );
   }
 
+  // Handler for removing a specific product from the cart
   FutureOr<void> _removeFromCart(
       _RemoveFromCart event, Emitter<CartState> emit) async {
     state.whenOrNull(
       loaded: (cartItems) async {
+        // Remove the specified product
         final newCartItems = [
           ...cartItems.where((element) => element.productId != event.productId)
         ];
-        final result = await _saveCartUsecase(newCartItems);
+        final result =
+            await _saveCartUsecase(newCartItems); // Save updated cart
         result.fold(
           (error) {
-            emit(CartState.error(message: error.message));
+            emit(CartState.error(message: error.message)); // Emit error state
           },
           (cartItems) {
-            emit(CartState.loaded(cartItems: newCartItems));
+            emit(
+                CartState.loaded(cartItems: newCartItems)); // Emit loaded state
           },
         );
       },
